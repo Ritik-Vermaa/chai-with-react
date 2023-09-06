882cc359f1d8baa0a87ab47ee7f42fbf4c53348d
RITIK VERMA <vermaritik933@gmail.com>
2023-09-06 11:19:53 +0530

    Props

--- 03viteTailwindprops/src/component/Card.jsx
+++ 03viteTailwindprops/src/component/Card.jsx
@@ -19,7 +19,7 @@ function Card({username , btntext="visit me"}) {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
         debitis?
       </p>
-      <button className="mt-2 inline-flex cursor-pointer items-center text-black font-semibold text-white">
+      <button className="mt-2 inline-flex cursor-pointer items-center  font-semibold text-black">
         {btntext}
       </button>
     </div>
